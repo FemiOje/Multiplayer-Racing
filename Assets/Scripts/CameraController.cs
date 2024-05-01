92a9c1123a3ceb5bd9c8e764e33dc711a35d5830
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-05-01 23:32:15 +0100

    fix camera rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,28 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     Camera _camera;
-    [SerializeField] Transform[] cameraPositions;
-    Vector2 _rightStickInputValue;
+
+    [SerializeField]
+    Transform[] cameraPositions;
     int _cameraIndex = 0;
-    [SerializeField] float _rotationSpeed = 40f;
     InputAction _cameraRotateAction;
+    Vector2 _rightStickInputValue;
+
     public Transform player;
     Rigidbody _playerRb;
     public Vector3 offset;
-    [SerializeField] float _lerpSpeed;
+
+    [SerializeField]
+    float _lerpSpeed;
+
+    [SerializeField]
+    float _cameraRotateSpeed;
 
     private void OnEnable()
     {
-        _cameraRotateAction = player.gameObject.GetComponent<PlayerInput>().actions.FindAction("RotateCamera");
+        _cameraRotateAction = player.gameObject
+            .GetComponent<PlayerInput>()
+            .actions.FindAction("RotateCamera");
     }
 
     void Start()
@@ -28,8 +37,8 @@ public class CameraController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        UpdateCameraRotation();
         UpdateCameraPosition();
-        // RotateCamera();
     }
 
     private void UpdateCameraPosition()
@@ -43,42 +52,30 @@ public class CameraController : MonoBehaviour
         transform.LookAt(player);
     }
 
+    private void UpdateCameraRotation()
+    {
+        _rightStickInputValue = _cameraRotateAction.ReadValue<Vector2>();
+
+        if (_rightStickInputValue.magnitude != 0)
+        {
+            transform.RotateAround(
+                player.transform.position,
+                Vector3.up,
+                _rightStickInputValue.x * _cameraRotateSpeed * Time.deltaTime
+            );
+            transform.RotateAround(
+                player.transform.position,
+                Vector3.right,
+                _rightStickInputValue.y * _cameraRotateSpeed * Time.deltaTime
+            );
+        }
+    }
+
     public void ToggleCameras(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
             IncreaseCameraIndex();
-        }
-    }
-
-    private void RotateCamera()
-    {
-        if (_cameraRotateAction != null)
-        {
-            _rightStickInputValue = _cameraRotateAction.ReadValue<Vector2>();
-
-            // Apply dead zone to input values
-            if (_rightStickInputValue.magnitude < 0.1f)
-            {
-                _rightStickInputValue = Vector2.zero;
-            }
-
-            if (_rightStickInputValue != Vector2.zero)
-            {
-                // Rotate the camera around the car based on input
-                float rotationX = _rightStickInputValue.y * _rotationSpeed * Time.deltaTime;
-                float rotationY = _rightStickInputValue.x * _rotationSpeed * Time.deltaTime;
-
-                // Calculate current angles of the camera
-                Vector3 currentAngles = _camera.gameObject.transform.eulerAngles;
-
-                // Apply rotation limits
-                float newXAngle = Mathf.Clamp(currentAngles.x - rotationX, 5f, 80f); // Adjust the min and max values as needed
-                float newYAngle = currentAngles.y + rotationY;
-
-                // Apply rotation to camera
-                _camera.gameObject.transform.rotation = Quaternion.Euler(newXAngle, newYAngle, 0f);
-            }
         }
     }
 
